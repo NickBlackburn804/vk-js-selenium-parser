0c14d3a06a83f9087b0cef878c0515f47542cd6c
NickBlackburn804 <wgs.teamm@gmail.com>
2021-02-09 13:23:07 +0500

    Update 4chParser.js

--- 4chParser.js
+++ 4chParser.js
@@ -5,9 +5,22 @@ const fs = require('fs');
     const browser = await firefox.launch({
         headless: false
     });
+    let images = [];
+    let boards = [];
     const context = await browser.newContext();
     const page = await context.newPage();
-        await page.goto('https://boards.4chan.org/s/');
+    const content = await page.content();
+    await page.goto('https://www.4chan.org/')
+        let $2 = cheerio.load(content)
+    await Promise.all([
+    $2('.boardlink').each((idx, elem) => {
+        let link = $2(elem);
+        boards.push(link.attr('href'));
+    })
+])
+
+    for(let board in boards){
+        await page.goto(boards[board]);
         let links = [];
         const content = await page.content();
         let $ = cheerio.load(content);
@@ -16,33 +29,38 @@ const fs = require('fs');
             links.push('https://boards.4chan.org/s/' + link.attr('href'));
         })
         fs.writeFileSync('4chLinks.txt', links)
-        //let images = [];
         let Threads = {};
+
         async function getThread() {
             const threadContent = await page.content();
             let $1 = cheerio.load(threadContent);
-            let opMessage = $1('.post op').text();
+            let opMessage = $1('.postMessage').text();
             let comments = [];
-            $('post reply').each((idx, elem) => {
+            $('.post reply').each((idx, elem) => {
                 let comment = $(elem).text();
                 comments.push(comment);
             })
-            // $('.desktop').each((i, e) => {
-            //     let link = $(e).attr('href');
-            //     images.push(link);
-            // })
+            $('.fileText a').each((i, e) => {
+                let link = $(e).attr('href');
+                images.push('\n' + link.replace('//', ''));
+            })
             Threads = {
                 opMessage: opMessage,
                 comments: comments
             }
         }
+
         for (let key in links) {
             await page.goto(links[key]);
             await getThread();
         }
         Threads = JSON.stringify(Threads);
-        fs.writeFileSync( '4chan' + '.txt', Threads)
-        // fs.writeFileSync( board + '_imgs.txt', images)
+        fs.writeFileSync(board + '4chan' + '.txt', Threads)
+        fs.writeFileSync(board + '_imgs4.txt', images)
+    }
+
+
+
     await page.close();
     await context.close();
     await browser.close();
